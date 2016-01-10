4f4c158a816b1ea03cb240c96ba9c9a91f29a833
vileda <vileda@vileda.cc>
2016-01-10 13:56:35 +0100

    added LoginView tests

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import CoreLayout from 'layouts/CoreLayout';
 import AccountsView from 'views/AccountsView';
-import AboutView from 'views/AboutView';
 import LoginView from 'views/LoginView';
 import DonateView from 'views/DonateView';
 import {requireAuthentication} from '../components/AuthenticatedComponent';
@@ -11,7 +10,6 @@ export default (
   <Route path='/' component={CoreLayout}>
     <IndexRoute component={requireAuthentication(AccountsView)}/>
     <Route path='/login' component={LoginView} />
-    <Route path='/about' component={requireAuthentication(AboutView)} />
     <Route path='/donate' component={requireAuthentication(DonateView)}/>
   </Route>
 );
