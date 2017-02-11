6fee70b5a4dd251c903679499c423342c1ab419d
derveloper <vileda+github@vileda.cc>
2017-02-11 17:55:59 +0100

    fix websocktUrl

--- donatr-ui/src/main/scala/donatrui/Websocket.scala
+++ donatr-ui/src/main/scala/donatrui/Websocket.scala
@@ -1,5 +1,6 @@
 package donatrui
 
+import org.scalajs.dom
 import org.scalajs.dom.raw.WebSocket
 
 import scala.scalajs.js
@@ -7,7 +8,10 @@ import scala.scalajs.js.JSON
 
 object Websocket {
   def init(): Unit = {
-    val ws = new WebSocket("ws://localhost:8080/events")
+    val schema =  if(dom.window.location.port == "") "wss://" else "ws://"
+    val port = if(dom.window.location.port == "") "" else ":"+dom.window.location.port
+    val wss = schema+dom.window.location.hostname+port+"/api/events"
+    val ws = new WebSocket(wss)
     ws.onmessage = (msg) => {
       val jsonData = msg.data.asInstanceOf[String]
       val parsedObj = JSON.parse(jsonData)
